ebcba5cb03a25c8921e3f7305806d12050813fc9
Bayarkhuu Purevgaram <bayarkhuu0512@gmail.com>
2015-02-25 13:39:36 +0800

    Splashscreen created

--- app/src/main/java/materialhappy/happybaby/com/materialhappy/MainActivity.java
+++ app/src/main/java/materialhappy/happybaby/com/materialhappy/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
@@ -16,17 +17,22 @@ import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonArrayRequest;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
 import materialhappy.happybaby.com.materialhappy.materialhappy.happybaby.com.materialhappy.contact.ContactFragment;
+import materialhappy.happybaby.com.materialhappy.materialhappy.happybaby.com.materialhappy.utils.Titanic;
 
 
 public class MainActivity extends Activity
@@ -49,6 +55,13 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+// Check if we're running on Android 5.0 or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // Call some material design APIs here
+
+        } else {
+            // Implement this feature without material design
+        }
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
@@ -60,58 +73,6 @@ public class MainActivity extends Activity
 
         Log.d(TAG,"OnCreate()");
 
-        JsonArrayRequest req = new JsonArrayRequest("http://happybaby.mn/app/contacts.php",
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        Log.d(TAG, response.toString());
-                        Toast.makeText(getApplicationContext(),
-                                response.toString(), Toast.LENGTH_LONG).show();
-//                        try {
-//                            // Parsing json array response
-//                            // loop through each json object
-//                            jsonResponse = "";
-//                            for (int i = 0; i < response.length(); i++) {
-//
-//                                JSONObject person = (JSONObject) response
-//                                        .get(i);
-//
-//                                String name = person.getString("name");
-//                                String email = person.getString("email");
-//                                JSONObject phone = person
-//                                        .getJSONObject("phone");
-//                                String home = phone.getString("home");
-//                                String mobile = phone.getString("mobile");
-//
-//                                jsonResponse += "Name: " + name + "\n\n";
-//                                jsonResponse += "Email: " + email + "\n\n";
-//                                jsonResponse += "Home: " + home + "\n\n";
-//                                jsonResponse += "Mobile: " + mobile + "\n\n\n";
-//
-//                            }
-//
-//                            txtResponse.setText(jsonResponse);
-//
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                            Toast.makeText(getApplicationContext(),
-//                                    "Error: " + e.getMessage(),
-//                                    Toast.LENGTH_LONG).show();
-//                        }
-
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d(TAG, "Error: " + error.getMessage());
-                Toast.makeText(getApplicationContext(),
-                        error.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        });
-
-
-
-        HappyApplication.getInstance().getRequestQueue().add(req);
 
     }
 
