05bafafc8807aa9f536cec98dd26a975ebfb60b2
Bayarkhuu Purevgaram <bayarkhuu0512@gmail.com>
2015-03-02 22:56:16 +0800

    Contact detail view added.

---
+++ app/src/main/java/materialhappy/happybaby/com/materialhappy/materialhappy/happybaby/com/materialhappy/contact/ContactDetailActivity.java
@@ -0,0 +1,17 @@
+package materialhappy.happybaby.com.materialhappy.materialhappy.happybaby.com.materialhappy.contact;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import materialhappy.happybaby.com.materialhappy.R;
+
+/**
+ * Created by Aagii on 3/2/2015.
+ */
+public class ContactDetailActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_detail);
+    }
+}
